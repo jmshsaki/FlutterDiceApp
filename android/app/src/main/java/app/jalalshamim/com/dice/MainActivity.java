ae9ddef3dbe4514111385e2765fdfe11ea26cba3
Jalal Muhammad Shamim <jmshsaki@gmail.com>
2022-02-14 02:54:53 +0600

    Finished
    A Simple DiceApp

---
+++ android/app/src/main/java/app/jalalshamim/com/dice/MainActivity.java
@@ -0,0 +1,6 @@
+package app.jalalshamim.com.dice;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DiceApp());
+}
+
+class DiceApp extends StatelessWidget {
+  const DiceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Dice(),
+      ),
+    );
+  }
+}
+
+class Dice extends StatefulWidget {
+  const Dice({Key? key}) : super(key: key);
+
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
